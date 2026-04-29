9957035a24126db6c1e73e676b26c5f0c924895c
AlulaT <alulabelay013@gmail.com>
2026-04-29 09:51:49 +0300

    Add package declaration and main method to app.java

--- app.java
+++ app.java
@@ -1,1 +1,12 @@
-
+package com.valaxy;
+/**
+* Hello World !
+*
+*/
+public class app 
+  {
+    public static void main ( string[] args )
+    {
+      system.out.println("Wellcom to Devops training");
+    }
+  }
